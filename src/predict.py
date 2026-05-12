5d48ef75cda1ba890189675904d2d0f528175b57
princessElza <elzsir@gmail.com>
2026-05-12 15:16:59 +0300

    feat: добавлен predict.py и финальные правки

--- src/predict.py
+++ src/predict.py
@@ -1,16 +1,10 @@
 import argparse
 import configparser
-from datetime import datetime
 import os
-import json
-import pandas as pd
+import numpy as np
 import pickle
-from sklearn.preprocessing import StandardScaler
-import shutil
 import sys
-import time
 import traceback
-import yaml
 
 from logger import Logger
 
@@ -24,88 +18,36 @@ class Predictor():
         self.config = configparser.ConfigParser()
         self.log = logger.get_logger(__name__)
         self.config.read("config.ini")
+        
         self.parser = argparse.ArgumentParser(description="Predictor")
-        self.parser.add_argument("-m",
-                                 "--model",
-                                 type=str,
-                                 help="Select model",
-                                 required=True,
-                                 default="LOG_REG",
-                                 const="LOG_REG",
-                                 nargs="?",
-                                 choices=["LOG_REG", "RAND_FOREST", "KNN", "GNB", "SVM", "D_TREE"])
-        self.parser.add_argument("-t",
-                                 "--tests",
-                                 type=str,
-                                 help="Select tests",
-                                 required=True,
-                                 default="smoke",
-                                 const="smoke",
-                                 nargs="?",
-                                 choices=["smoke", "func"])
-        self.X_train = pd.read_csv(
-            self.config["SPLIT_DATA"]["X_train"], index_col=0)
-        self.y_train = pd.read_csv(
-            self.config["SPLIT_DATA"]["y_train"], index_col=0)
-        self.X_test = pd.read_csv(
-            self.config["SPLIT_DATA"]["X_test"], index_col=0)
-        self.y_test = pd.read_csv(
-            self.config["SPLIT_DATA"]["y_test"], index_col=0)
-        self.sc = StandardScaler()
-        self.X_train = self.sc.fit_transform(self.X_train)
-        self.X_test = self.sc.transform(self.X_test)
-        self.log.info("Predictor is ready")
+        
+        # Загружаем тестовые данные
+        self.X_test = np.load(self.config["SPLIT_DATA"]["x_test"])
+        self.y_test = np.load(self.config["SPLIT_DATA"]["y_test"])
+        
+        self.log.info(f"Predictor is ready. Test data shape: {self.X_test.shape}")
 
     def predict(self) -> bool:
         args = self.parser.parse_args()
+        
+        # Загружаем модель (просто из секции MODEL)
         try:
-            classifier = pickle.load(
-                open(self.config[args.model]["path"], "rb"))
+            model_path = self.config["MODEL"]["model_path"]
+            classifier = pickle.load(open(model_path, "rb"))
+            self.log.info(f"Model loaded from {model_path}")
         except FileNotFoundError:
             self.log.error(traceback.format_exc())
             sys.exit(1)
-        if args.tests == "smoke":
-            try:
-                score = classifier.score(self.X_test, self.y_test)
-                print(f'{args.model} has {score} score')
-            except Exception:
-                self.log.error(traceback.format_exc())
-                sys.exit(1)
-            self.log.info(
-                f'{self.config[args.model]["path"]} passed smoke tests')
-        elif args.tests == "func":
-            tests_path = os.path.join(os.getcwd(), "tests")
-            exp_path = os.path.join(os.getcwd(), "experiments")
-            for test in os.listdir(tests_path):
-                with open(os.path.join(tests_path, test)) as f:
-                    try:
-                        data = json.load(f)
-                        X = self.sc.transform(
-                            pd.json_normalize(data, record_path=['X']))
-                        y = pd.json_normalize(data, record_path=['y'])
-                        score = classifier.score(X, y)
-                        print(f'{args.model} has {score} score')
-                    except Exception:
-                        self.log.error(traceback.format_exc())
-                        sys.exit(1)
-                    self.log.info(
-                        f'{self.config[args.model]["path"]} passed func test {f.name}')
-                    exp_data = {
-                        "model": args.model,
-                        "model params": dict(self.config.items(args.model)),
-                        "tests": args.tests,
-                        "score": str(score),
-                        "X_test path": self.config["SPLIT_DATA"]["x_test"],
-                        "y_test path": self.config["SPLIT_DATA"]["y_test"],
-                    }
-                    date_time = datetime.fromtimestamp(time.time())
-                    str_date_time = date_time.strftime("%Y_%m_%d_%H_%M_%S")
-                    exp_dir = os.path.join(exp_path, f'exp_{test[:6]}_{str_date_time}')
-                    os.mkdir(exp_dir)
-                    with open(os.path.join(exp_dir,"exp_config.yaml"), 'w') as exp_f:
-                        yaml.safe_dump(exp_data, exp_f, sort_keys=False)
-                    shutil.copy(os.path.join(os.getcwd(), "logfile.log"), os.path.join(exp_dir,"exp_logfile.log"))
-                    shutil.copy(self.config[args.model]["path"], os.path.join(exp_dir,f'exp_{args.model}.sav'))
+        
+        # Проверяем точность
+        try:
+            score = classifier.score(self.X_test, self.y_test)
+            print(f'Model accuracy: {score:.4f}')
+            self.log.info(f'Model passed test with accuracy {score:.4f}')
+        except Exception:
+            self.log.error(traceback.format_exc())
+            sys.exit(1)
+        
         return True
 
 
